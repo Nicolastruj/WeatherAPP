5db9c14e344275bc909a1c803aeed6efeac254a9
Nicolas <nicolastrujillloestevez@gmail.com>
2024-01-07 18:40:53 +0000

    54th commit let him cook

--- GUI/src/main/java/org/ulpgc/dacd/control/SwingGUIExecutor.java
+++ GUI/src/main/java/org/ulpgc/dacd/control/SwingGUIExecutor.java
@@ -16,7 +16,6 @@ import java.util.stream.Collectors;
 
 public class SwingGUIExecutor implements GUIExecutor {
 
-    @Override
     public void execute() {
         createAndShowInitialGUI();
     }
@@ -71,6 +70,7 @@ public class SwingGUIExecutor implements GUIExecutor {
         islandFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         islandFrame.setLayout(new BorderLayout());
 
+        // Panel para la imagen de la isla
         JPanel imagePanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
@@ -89,53 +89,82 @@ public class SwingGUIExecutor implements GUIExecutor {
         islandFrame.add(imagePanel, BorderLayout.CENTER);
 
         JButton backButton = new JButton("Reversa");
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                islandFrame.setVisible(false);
-                createAndShowInitialGUI();
-            }
+        backButton.addActionListener(e -> {
+            islandFrame.setVisible(false);
+            createAndShowInitialGUI();
         });
         islandFrame.getContentPane().add(backButton, BorderLayout.SOUTH);
 
-        JPanel legendPanel = new JPanel();
-        legendPanel.setLayout(new BorderLayout());
+        // Crear contenedores para los paneles de clima y hoteles
+        JPanel weatherPanelContainer = new JPanel(new BorderLayout());
+        JPanel hotelPanelContainer = new JPanel(new BorderLayout());
+
+        // Crear CardLayout para el clima y los hoteles
+        CardLayout weatherCardLayout = new CardLayout();
+        CardLayout hotelCardLayout = new CardLayout();
+
+        JPanel weatherLegendPanel = new JPanel(weatherCardLayout);
+        JPanel hotelLegendPanel = new JPanel(hotelCardLayout);
+
         List<JPanel> weatherPanels = showWeatherData(islandName);
-        final int[] currentPanelIndex = {0};
-        legendPanel.add(weatherPanels.get(currentPanelIndex[0]), BorderLayout.CENTER);
+        List<JPanel> hotelEventPanels = showHotelEventPanels(islandName);
 
-        JButton prevButton = new JButton("<");
-        JButton nextButton = new JButton(">");
-        prevButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (currentPanelIndex[0] > 0) {
-                    currentPanelIndex[0]--;
-                    legendPanel.removeAll();
-                    legendPanel.add(weatherPanels.get(currentPanelIndex[0]), BorderLayout.CENTER);
-                    legendPanel.revalidate();
-                    legendPanel.repaint();
-                }
-            }
-        });
-        nextButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (currentPanelIndex[0] < weatherPanels.size() - 1) {
-                    currentPanelIndex[0]++;
-                    legendPanel.removeAll();
-                    legendPanel.add(weatherPanels.get(currentPanelIndex[0]), BorderLayout.CENTER);
-                    legendPanel.revalidate();
-                    legendPanel.repaint();
-                }
-            }
+        for (JPanel weatherPanel : weatherPanels) {
+            weatherLegendPanel.add(weatherPanel, "Weather");
+        }
+
+        for (JPanel hotelPanel : hotelEventPanels) {
+            hotelLegendPanel.add(hotelPanel, "Hotel");
+        }
+
+        weatherPanelContainer.add(weatherLegendPanel, BorderLayout.CENTER);
+        hotelPanelContainer.add(hotelLegendPanel, BorderLayout.CENTER);
+
+        islandFrame.add(weatherPanelContainer, BorderLayout.WEST);
+        islandFrame.add(hotelPanelContainer, BorderLayout.EAST);
+
+        // Crear botones de navegación para el clima
+        JButton prevWeatherButton = new JButton("<");
+        JButton nextWeatherButton = new JButton(">");
+
+        prevWeatherButton.addActionListener(e -> {
+            CardLayout cardLayout = (CardLayout) weatherLegendPanel.getLayout();
+            cardLayout.previous(weatherLegendPanel);
         });
 
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 0)); // Usamos FlowLayout para centrar los botones y darles un pequeño margen horizontal
-        buttonPanel.add(prevButton);
-        buttonPanel.add(nextButton);
-        islandFrame.add(buttonPanel, BorderLayout.NORTH);
-        islandFrame.add(legendPanel, BorderLayout.EAST);
+        nextWeatherButton.addActionListener(e -> {
+            CardLayout cardLayout = (CardLayout) weatherLegendPanel.getLayout();
+            cardLayout.next(weatherLegendPanel);
+        });
+
+        JPanel weatherButtonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
+        weatherButtonPanel.add(prevWeatherButton);
+        weatherButtonPanel.add(nextWeatherButton);
+
+        // Crear botones de navegación para los hoteles
+        JButton prevHotelButton = new JButton("<");
+        JButton nextHotelButton = new JButton(">");
+
+        prevHotelButton.addActionListener(e -> {
+            CardLayout cardLayout = (CardLayout) hotelLegendPanel.getLayout();
+            cardLayout.previous(hotelLegendPanel);
+        });
+
+        nextHotelButton.addActionListener(e -> {
+            CardLayout cardLayout = (CardLayout) hotelLegendPanel.getLayout();
+            cardLayout.next(hotelLegendPanel);
+        });
+
+        JPanel hotelButtonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 0));
+        hotelButtonPanel.add(prevHotelButton);
+        hotelButtonPanel.add(nextHotelButton);
+
+        // Agregar los botones de navegación al área norte del JFrame
+        JPanel navigationPanel = new JPanel(new BorderLayout());
+        navigationPanel.add(weatherButtonPanel, BorderLayout.WEST);
+        navigationPanel.add(hotelButtonPanel, BorderLayout.EAST);
+
+        islandFrame.add(navigationPanel, BorderLayout.NORTH);
 
         islandFrame.setVisible(true);
     }
@@ -169,34 +198,69 @@ public class SwingGUIExecutor implements GUIExecutor {
         }
         return weatherPanels;
     }
+
+    private List<JPanel> showHotelEventPanels(String islandName) {
+        List<JPanel> hotelEventPanels = new ArrayList<>();
+        List<String> recentTables = getRecentTables();
+
+        try {
+            String databasePath = "Business-Unit/src/main/resources/databases/HotelEventsMart.db";
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:" + databasePath);
+
+            for (String tableName : recentTables) {
+                if (tableName.startsWith(islandName)) {
+                    String query = "SELECT * FROM " + tableName;
+                    Statement stmt = conn.createStatement();
+                    ResultSet rs = stmt.executeQuery(query);
+
+                    while (rs.next()) {
+                        JPanel hotelEventPanel = new JPanel(new GridLayout(0, 1));
+                        hotelEventPanel.add(new JLabel("Nombre: " + rs.getString("name")));
+                        hotelEventPanel.add(new JLabel("Ubicación: " + rs.getString("location")));
+                        hotelEventPanel.add(new JLabel("Precio: " + rs.getDouble("price") + " €"));
+                        hotelEventPanel.add(new JLabel("Valoración: " + rs.getString("review")));
+                        hotelEventPanel.add(new JLabel("Servicios: " + rs.getString("services")));
+                        hotelEventPanels.add(hotelEventPanel);
+                    }
+
+                    rs.close();
+                    stmt.close();
+                }
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al obtener datos de eventos de hoteles: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return hotelEventPanels;
+    }
+
+
     private List<String> getRecentTables() {
         List<String> recentTables = new ArrayList<>();
         try {
-            String databasePath = "Business-Unit/src/main/resources/databases/WeatherEventsMart.db";
+            String databasePath = "Business-Unit/src/main/resources/databases/HotelEventsMart.db";
             Connection conn = DriverManager.getConnection("jdbc:sqlite:" + databasePath);
             DatabaseMetaData metaData = conn.getMetaData();
             ResultSet tables = metaData.getTables(null, null, null, new String[]{"TABLE"});
 
-            // Determinar la fecha de mañana
             LocalDate tomorrow = LocalDate.now().plusDays(1);
             String tomorrowDate = tomorrow.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
-            // Map para mantener un seguimiento de las tablas y sus fechas y horas
             Map<String, LocalDateTime> tableDateTimes = new HashMap<>();
 
             while (tables.next()) {
                 String tableName = tables.getString("TABLE_NAME");
                 if (tableName.contains(tomorrowDate)) {
-                    // Extraer la hora del nombre de la tabla
                     int startIndex = tableName.lastIndexOf("_") + 1;
-                    int endIndex = tableName.lastIndexOf(".");
+                    int endIndex = tableName.lastIndexOf("_", startIndex) + 3; // Agregar 3 para incluir las dos cifras de la hora
                     String timeString = tableName.substring(startIndex, endIndex);
-                    LocalDateTime dateTime = LocalDateTime.parse(tomorrowDate + "_" + timeString, DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+                    LocalDateTime dateTime = LocalDateTime.parse(tomorrowDate + "_" + timeString, DateTimeFormatter.ofPattern("yyyyMMdd_HH"));
                     tableDateTimes.put(tableName, dateTime);
                 }
             }
 
-            // Ordenar las tablas por fecha y hora
             recentTables = tableDateTimes.entrySet().stream()
                     .sorted(Map.Entry.<String, LocalDateTime>comparingByValue().reversed())
                     .limit(8)
